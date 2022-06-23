35c25e8c65ab292f80dfe6b968365ef561212f62
ahmedsport <ahmedsport@hotmail.com>
2022-06-23 15:13:07 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
+package src;
+
 public class Main {
 
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
+        char studentFirstInitial = 'A';
+        char studentLastInitial = 'E';
         boolean hasPerfectAttendance = true;
+        String studentFirstName = "Ahmed";
+        String studentLastName = "El-Atreby";
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
     }
 }
