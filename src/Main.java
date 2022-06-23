ded4abc567b609b680ff9da0d2f300c68169dfc3
ahmedsport <ahmedsport@hotmail.com>
2022-06-23 15:20:37 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+package src;
+
 public class Main {
 
     public static void main(String[] args) {
         double studentGPA = 3.45;
-        String studentFirstName = "Kayla";
-        String studentLastName = "Hammon";
+        String studentFirstName = "Ahmed";
+        String studentLastName = "El-Atreby";
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
     }
 }
