c34ed20e4194f3b3d150435a233e032634743f18
ahmedsport <ahmedsport@hotmail.com>
2022-06-29 14:39:56 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
+package src;
+
 import java.util.Scanner;
 
 public class Main {
 
-    public static void calculateTotalMealCost() {
+    public static void calculateTotalMealCost(double listedMealPrice,
+                                              double tipRate,
+                                              double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+        System.out.println("Your total meal price is " + result);
+
         
     }
 
     public static void main(String[] args) {
+        calculateTotalMealCost(15,.2,.08);
 
     }
+
+
 
 }
