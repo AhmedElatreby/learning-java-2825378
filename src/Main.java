ac757a326a63f50b7e3a3dbb82dd40128e6aab26
ahmedsport <ahmedsport@hotmail.com>
2022-07-01 10:54:57 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-<<<<<<< HEAD
+package src;<<<<<<< HEAD
+package src;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Triangle triangleA = new Triangle(15,8,15,8,17);
+        Triangle triangleB = new Triangle(3,2.598,3,3,3);
 =======
-package src;
+package Object src;
+        src;
 
 public class Main {
 
